2214821c2e8750ed881e2d2e3b21e73cbbba071b
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-19 14:04:04 -0400

    added d3 to md for chart, adding api to fun

--- assets/js/fun.js
+++ assets/js/fun.js
@@ -1,23 +1,11 @@
-var queryCat = "https://anapioficeandfire.com/api/characters/583";
-
-$.ajax({
-    url: queryCat,
-    method: "GET"
-})
-    .then(function (response) {
-        console.log(response[0]);
-        console.log(response[0].positiveIncrease);
-    });
-
-
-// var queryChart = "https://quickchart.io/chart?c=[[insert chart config]].";
+// var queryCat = "https://api.thecatapi.com/images/search"; 56c151e1 - 0419 - 430a - 85f1 - 85b5a51c2175
 
 // $.ajax({
-//     url: queryChart,
+//     url: queryCat,
 //     method: "GET"
 // })
 //     .then(function (response) {
-//         console.log(response[0]);
-//         console.log(response[0].positiveIncrease);
+//         console.log(response);
+
 //     });
 
