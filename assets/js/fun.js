10d580c4aae0f4ee5063182951f97aa68cb50607
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-20 01:33:21 -0400

    added cat API, fixed CSS on VA, added darkmode

--- assets/js/fun.js
+++ assets/js/fun.js
@@ -1,14 +1,32 @@
-var queryCat = "https://api.thecatapi.com/v1/images/search?limit=5&page=10&order=Desc";
+// created variable page = 1 to get the API to switch through cat pictures instead of only choosing one image
+var page = 1
 
-$.ajax({
-    headers: {
-        "x-api-key": "56c151e1-0419-430a-85f1-85b5a51c2175"
-    },
-    url: queryCat,
-    method: "GET"
-})
-    .then(function (response) {
-        console.log(response);
 
-    });
+$("#cat-API").click(function () {
+    // added + page here to include the var above
+    var queryCat = "https://api.thecatapi.com/v1/images/search?limit=5&page=" + page + "&order=Desc";
 
+    $.ajax({
+        headers: {
+            "x-api-key": "56c151e1-0419-430a-85f1-85b5a51c2175"
+        },
+        url: queryCat,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response[0].url);
+            // set attribute
+            $("#catPicture").attr("src", response[0].url)
+
+        });
+    // added page ++ so the var will change images
+    page++
+
+});
+
+
+
+$("#darkMode").click(function () {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+});
